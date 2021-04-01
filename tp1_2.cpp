114fc783b0045060b2de8a6a0df7e35c039e6ba5
Emilio Merchan <emimerchan83@gmail.com>
2021-04-01 17:18:13 -0300

    Se termino el punto

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -3,10 +3,14 @@
 int cuadradoNumero(int numero);
 void cuadradoNumV(int *numero);
 void mostrarDirYCont(int *numero);
+void Invertir(int *a, int *b);
+void Orden(int *a, int *b);
+
 
 int main(){
     printf("Escriba un numero para calcular su cuadrado \n");
     int numero;
+    int a, b;
     scanf("%d", &numero);                           //Pasaje por valor
     printf("%d", cuadradoNumero(numero));
 
@@ -18,6 +22,16 @@ int main(){
     printf("\n");
 
     mostrarDirYCont(&numero);   //Funcion mostrar dir y contenido
+
+    printf("\n");
+
+    printf("Invertir numeros a y b, escriba el valor de A\n");
+    scanf("%d", &a);
+    printf("Escriba el valor de B\n");
+    scanf("%d", &b);
+    Orden(&a, &b);
+    printf("El valor de A ahora es %d, y el de B es %d", a, b);
+
 
     return 0;   
 }
@@ -37,4 +51,19 @@ void mostrarDirYCont(int *numero){
     printf("Direccion de memoria de la variable: %p\n", &(*numero));
     printf("Contenido de la variable: %d", *numero);
 
+}
+
+
+void Invertir(int *a, int *b){
+    int aux;
+    aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void Orden(int *a, int *b){
+    if (*a > *b)
+    {
+        Invertir(a,b);
+    }
 }
